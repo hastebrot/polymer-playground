42d87367e9f635cd7289d2225f0d34d9d1828de7
Benjamin Gudehus <hastebrot@gmail.com>
2017-09-06 20:56:26 +0200

    (style) Define EditorConfig file.

--- behaviors-and-observers/test/index.js
+++ behaviors-and-observers/test/index.js
@@ -1,16 +1,17 @@
 describe("simple element with behavior", () => {
-    let SimpleElement;
-    before(() => {
-        SimpleElement = Polymer({
-            is: "simple-element",
-        })
+  let SimpleElement
+  before(() => {
+    // given:
+    SimpleElement = Polymer({
+      is: "simple-element",
     })
+  })
 
-    it("should create simple-element", () => {
-        // when:
-        const element = new SimpleElement()
+  it("should create simple-element", () => {
+    // when:
+    const element = new SimpleElement()
 
-        // then:
-        expect(element.is).to.equal("simple-element")
-    })
+    // then:
+    expect(element.is).to.equal("simple-element")
+  })
 })
