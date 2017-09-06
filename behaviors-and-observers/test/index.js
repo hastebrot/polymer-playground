51895b280668a745bb8a837ef790fc6c8d544227
Benjamin Gudehus <hastebrot@gmail.com>
2017-09-06 22:25:29 +0200

    (test) Introduce string and object property.

--- behaviors-and-observers/test/index.js
+++ behaviors-and-observers/test/index.js
@@ -1,19 +1,37 @@
+// https://www.polymer-project.org/2.0/docs/devguide/observers#observing-path-changes
+
 describe("simple element with behavior", () => {
   let SimpleElement
-  let observerEvent
+  let lastTextValue
+  let lastModelValue
   before(() => {
     // given:
     SimpleElement = Polymer({
       is: "simple-element",
       properties: {
+        text: {
+          type: String,
+          value: function() { return "foo" },
+          observer: "_textChanged",
+        },
         model: {
           type: Object,
-          value: {}
+          value: function() { return {} }
         }
       },
-      observers: ["_observer(model.*)"],
-      _observer: function(event) {
-        observerEvent = event
+      observers: ["_textChanged(text)", "_modelChanged(model.*)"],
+      changeProperties: function() {
+        console.log("changeProperties()")
+        this.text = "bar"
+        this.set("model", { key: "value" })
+      },
+      _textChanged: function(newValue, oldValue) {
+        console.log("_textChanged(")
+        lastTextValue = newValue
+      },
+      _modelChanged: function(event) {
+        console.log("_modelChanged()")
+        lastTextValue = event.newValue
       }
     })
   })
@@ -29,8 +47,10 @@ describe("simple element with behavior", () => {
   it("should call observer", () => {
     // when:
     const element = new SimpleElement()
+    element.changeProperties()
 
     // then:
-    expect(observerEvent).to.deep.equal({ path: "model", value: {}, base: {} })
+    expect(lastModelValue).to.deep.equal({ path: "model", value: {}, base: {} })
+    expect(lastTextValue).to.deep.equal({ path: "text", value: {}, base: {} })
   })
 })
