b73a9a0059d65588ef49a0a228c94453a54ddec5
Benjamin Gudehus <hastebrot@gmail.com>
2017-09-06 21:19:47 +0200

    (test) Use observer with Polymer 1.4.

--- behaviors-and-observers/test/index.js
+++ behaviors-and-observers/test/index.js
@@ -1,9 +1,20 @@
 describe("simple element with behavior", () => {
   let SimpleElement
+  let observerEvent
   before(() => {
     // given:
     SimpleElement = Polymer({
       is: "simple-element",
+      properties: {
+        model: {
+          type: Object,
+          value: {}
+        }
+      },
+      observers: ["_observer(model.*)"],
+      _observer: function(event) {
+        observerEvent = event
+      }
     })
   })
 
@@ -13,5 +24,13 @@ describe("simple element with behavior", () => {
 
     // then:
     expect(element.is).to.equal("simple-element")
+  })
+
+  it("should call observer", () => {
+    // when:
+    const element = new SimpleElement()
+
+    // then:
+    expect(observerEvent).to.deep.equal({ path: "model", value: {}, base: {} })
   })
 })
